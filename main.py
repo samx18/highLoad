35d727b7d72b08dc1e442f6b357a2d2a65958a54
Sam <sampat.palani@gmail.com>
2018-04-17 11:52:24 -0700

    Added slack integration

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
-from urllib2 import  Request, urlopen, URLError, HTTPError
-from subprocess import call
-ENCRYPTED_HOOK_URL = 'AQECAHgT/MsA4/sL3S5flIV8eLI13GPSUwLOdlMiunWVHtJm/QAAAKcwgaQGCSqGSIb3DQEHBqCewqjkhhdsaNcvpwhgalpPcG/zOdFIdFOunHYCARCAYMZU9PoYrTUEAkey+GCXqLTTmNpECB2Sh6CY0hwh1b2gV3+0GxRMwtvZiNYltvSXjR2pY2qoxxmXfxH54jzsOBUNOtICPzj4onrmbmd6d4nfMiLtuGUwT1rwctPIHifCKQ=='
-import simplejson as json
-SLACK_CHANNEL = 'bot-testing'
+ffrom urllib2 import  Request, urlopen, URLError, HTTPError
+import subprocess
+import os
 
-call(["ls", "-l"])
+# Set slack integration
+
+SLACK_CHANNEL = 'bot-testing'
+URL="https://hooks.slack.com/services/Txxxxxxxxxxxxxx"
+
+# Set trigger threshold
+
+trigger=6.00
+
+# Get hostname from system
+
+process = subprocess.Popen(['hostname'], stdout=subprocess.PIPE)
+out, err = process.communicate()
+host=out.split('\n')[0]
+#host=out.split(' ')[0].split('\n')
+
+#host=call(["hostname"])
+
+print host
+
+# Read load averages
+
+f=open("/proc/loadavg","r")
+data=f.readlines()
+f.close()
+
+# Read the first line of the data list
+
+datastr=data[0]
+
+# Split the line into a sting list delimted by space and extract the first element
+
+load=datastr.split(' ')[0]
+
+cmd="curl -X POST -H 'Content-type: application/json' --data '{\"text\": \":rotating_light: There is a high load on server:` %s ` . Current Load: ` %s ` .\", \"channel\": \"#bot-testing\"}' https://hooks.slack.com/services/Txxxxxxxxxxxxxx -k" % (host,load)
+
+if float(load) < trigger: os.system(cmd)
